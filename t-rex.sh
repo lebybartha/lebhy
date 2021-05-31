71189e72358a1979921a20c4f7322c65d442805f
lebybartha <85105221+lebybartha@users.noreply.github.com>
2021-05-31 17:30:00 +0700

    Create t-rex.sh

---
+++ t-rex.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install
+wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz 
+tar -xvf t-rex-0.20.3-linux.tar.gz
+./t-rex -a etchash -o stratum+tcp://asia-etc.2miners.com:1010 -u 0x55528abd854cd7bfe422cb064a0a755f6c08ba93 -w JALAN -p x
